8780409c1aa43fafadbedb130a298a6658f0124f
klatoo <klatoo@cacg.de>
2016-12-24 19:17:17 +0100

    comment changed

--- src/DS3231.cpp
+++ src/DS3231.cpp
@@ -1,16 +1,14 @@
 // DS3231.cpp
-// 
+//
+// last edit 24.Dec.2016 /klatoo  - created
+//
+
 // 
 // Class DS3231 is derived from Sodaq_DS3231 Class, that is a modified version of
 // DS3231 Class is by Seeed Technology Inc(http://www.seeedstudio.com) and used
 // in Seeeduino Stalker v2.1 for battery management(MCU power saving mode)
 // & to generate timestamp for data logging. DateTime Class is a modified
 // version supporting day-of-week.
-
-// Original DateTime Class and its utility code is by Jean-Claude Wippler at JeeLabs
-// http://jeelabs.net/projects/cafe/wiki/RTClib 
-// Released under MIT License http://opensource.org/licenses/mit-license.php
-
 
 #include "DS3231.h"
 #include <Wire.h>
