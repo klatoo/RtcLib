e4b079a91e5c30f5baf7bd2cb8bbe85e4ae85c34
klatoo <klatoo@cacg.de>
2016-12-25 02:14:41 +0100

    updated keywords.txt; changed defines to const data members

--- src/RtcLibHelper.h
+++ src/RtcLibHelper.h
@@ -15,13 +15,20 @@
 // Timespan which can represent changes in time with seconds accuracy.
 class TimeSpan {
 public:
+	const long SECONDS_FROM_1970_TO_2000 946684800L;
+	const long SECONDS_PER_DAY = 86400L;
+	const int SECONDS_PER_HOUR = 3600;
+	const int SECONDS_PER_MINUTE = 60;
+	const int MINUTES_PER_HOUR = 60;
+	const int HOURS_PER_DAY = 24;
+
 	TimeSpan(int32_t seconds = 0);
 	TimeSpan(int16_t days, int8_t hours, int8_t minutes, int8_t seconds);
 	TimeSpan(const TimeSpan& copy);
-	int16_t days() const { return _seconds / 86400L; }
-	int8_t  hours() const { return _seconds / 3600 % 24; }
-	int8_t  minutes() const { return _seconds / 60 % 60; }
-	int8_t  seconds() const { return _seconds % 60; }
+	int16_t days() const { return _seconds / SECONDS_PER_DAY; }
+	int8_t  hours() const { return _seconds / SECONDS_PER_HOUR % HOURS_PER_DAY; }
+	int8_t  minutes() const { return _seconds / SECONDS_PER_MINUTE % MINUTES_PER_HOUR; }
+	int8_t  seconds() const { return _seconds % SECONDS_PER_MINUTE; }
 	int32_t totalseconds() const { return _seconds; }
 
 	TimeSpan operator+(const TimeSpan& right);
